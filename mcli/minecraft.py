9b38576200b5e5753477ac5c59588008644da32d
ello <elloradislovelinux@gmail.com>
2026-05-15 16:27:45 +0800

    Add `minecraft` module for Minecraft related stuff and `.Instance` for managing Minecraft instance.

---
+++ mcli/minecraft.py
@@ -0,0 +1,67 @@
+'''Module handling Minecraft related stuff.'''
+
+import re
+from os import path
+from mcli.modrinth import Loader
+
+class Instance:
+	'''An object holding a Minecraft instance properties.'''
+	path: str = ''
+	version: str = ''
+	loader: Loader = Loader.UNSUPPORTED
+
+	def __init__(self, folder: str) -> None:
+		self.path = folder
+		self.probe()
+
+	def probe(self) -> None:
+		'''Auto fill `self.version` and `self.loader` based of `self.path`.'''
+		latestlog = path.join(self.logfolder, 'latest.log')
+
+		if not path.exists(latestlog):
+			return
+
+		with open(latestlog, 'r', encoding='utf8') as log:
+			loghead = log.readline()
+
+			matchdat = re.search( # fabric log regex.
+				'^.*?Loading Minecraft (?P<mcver>.*?) with (?P<loader>.*?) Loader (?P<loaderver>.*?)$',
+				loghead
+			)
+			matchdat = re.search( # forge log regex.
+				'^.*?--version, (?P<mcver>.*?)-(?P<loader>.*?)-(?P<loaderver>.*?),.*?$',
+				loghead
+			) if not matchdat else matchdat
+
+			if matchdat:
+				self.version = matchdat.group('mcver')
+				self.loader = Loader(matchdat.group('loader').lower())
+
+	def is_valid(self) -> bool:
+		'''Return whether this instance is a Minecraft instance.'''
+		return path.exists(self.logfolder) and \
+			path.exists(self.savefolder) and \
+			path.exists(self.configfolder)
+
+	@property
+	def modfolder(self) -> str:
+		'''Mod folder.'''
+		return path.join(self.path, 'mods')
+
+	@property
+	def logfolder(self) -> str:
+		'''Log folder.'''
+		return path.join(self.path, 'logs')
+
+	@property
+	def savefolder(self) -> str:
+		'''Save folder.'''
+		return path.join(self.path, 'saves')
+
+	@property
+	def configfolder(self) -> str:
+		'''Config folder.'''
+		return path.join(self.path, 'config')
+
+	def __repr__(self) -> str:
+		return f'<MinecraftInstance {f'{self.loader}:{self.version}' if self.is_valid() else 'invalid'}>'
